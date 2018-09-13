4592a256582b69b5e74707c638a3c2bb1c0f874f
Desko Sekulović <desko.sekulovic@gmail.com>
2018-09-13 14:13:59 +0200

    Small bug fixed

--- src/App.js
+++ src/App.js
@@ -9,16 +9,15 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [],
-      todo: ''
+      todo: '',
+      show: ''
     }
   }
 
-  handleChange = e => {
-    this.setState({todo: e.target.value});
-  }
-  onSubmit = e => {
-    e.preventDefault();
-  }
+  handleChange = e => this.setState({todo: e.target.value});
+
+  onSubmit = e => e.preventDefault();
+
   addTodo = () => {
     if(this.state.todo.trim().length===0){
       this.setState({todo: ''});
@@ -28,34 +27,27 @@ class App extends Component {
       todos: [...this.state.todos, {
         todo: this.state.todo,
         id: Math.random(),
-        completed: false,
-        show: true
+        completed: false
       }],
       todo: ''
     });
   }
-  toggleCompleted = id => {
-    this.setState({todos: this.state.todos.map(el => el.id===id ? Object.assign({}, el, {completed: !el.completed}) : el)});
-  }
+  toggleCompleted = id => this.setState({todos: this.state.todos.map(el => el.id===id ? Object.assign({}, el, {completed: !el.completed}) : el)});
 
-  showUncompleted = () => {
-    this.setState({todos: this.state.todos.map(el => el.completed===true ? Object.assign({}, el, {show: false}) : Object.assign({}, el, {show: true}))});
-  }
-  showCompleted = () => {
-    this.setState({todos: this.state.todos.map(el => el.completed===false ? Object.assign({}, el, {show: false}) : Object.assign({}, el, {show: true}))});
-  }
-  showAll = () => {
-    this.setState({todos: this.state.todos.map(el => Object.assign({}, el, {show: true}))});
-  }
-  deleteTodo = id => {
-    this.setState({todos: this.state.todos.filter(el => el.id!==id && Object.assign({}, el))});
-  }
+  showUncompleted = () => this.setState({show:'uncompleted'});
+
+  showCompleted = () => this.setState({show:'completed'});
+
+  showAll = () => this.setState({show:''});
+
+  deleteTodo = id => this.setState({todos: this.state.todos.filter(el => el.id!==id && Object.assign({}, el))});
 
   render() {
+    const { todos, todo, show } = this.state;
     return (
       <div className="App">
-        <Header onSubmit={this.onSubmit} handleChange={this.handleChange} addTodo={this.addTodo} todo={this.state.todo} />
-        <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted} deleteTodo={this.deleteTodo} />
+        <Header onSubmit={this.onSubmit} handleChange={this.handleChange} addTodo={this.addTodo} todo={todo} />
+        <TodoList todos={todos} show={show} toggleCompleted={this.toggleCompleted} deleteTodo={this.deleteTodo} />
         <Footer showUncompleted={this.showUncompleted} showCompleted={this.showCompleted} showAll={this.showAll} />
       </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import Todo from './Todo.js';
 
-const TodoList = ({ todos, toggleCompleted, deleteTodo }) => {
+const TodoList = ({ todos, show, toggleCompleted, deleteTodo }) => {
+  if(show){
+    let completed;
+    completed = show==='uncompleted' && false;
+    completed = show==='completed' && true;
+    return(
+      <ul>
+        {todos.map(todo=> todo.completed===completed &&
+            (
+              <div key={todo.id}>
+                <Todo todo={todo} toggleCompleted={()=>toggleCompleted(todo.id)} />
+                <button id={todo.id} onClick={()=>deleteTodo(todo.id)}>Delete</button>
+              </div>
+          )
+
+        )}
+      </ul>
+    );
+  }
   return(
     <ul>
       {todos.map(todo=>{
-        if(todo.show){
           return (
             <div key={todo.id}>
               <Todo todo={todo} toggleCompleted={()=>toggleCompleted(todo.id)} />
               <button id={todo.id} onClick={()=>deleteTodo(todo.id)}>Delete</button>
             </div>
         )
-        }
       })}
     </ul>
   );
+
 }
 
 export default TodoList;
